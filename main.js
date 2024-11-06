6cad86f9732e94ec3be570d0107c82f2a1f2498c
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-11-06 09:50:42 -0300

    Entrega do desafio

---
+++ main.js
@@ -0,0 +1,27 @@
+let heroName = "ZéVituu"
+let xp = 2000
+
+if (xp <= 1000){
+    console.log("O Heroi de nome "+heroName+" está no nivel de " +"Ferro")
+}
+else if (xp >1000 && xp <= 2000){
+    console.log("O Heroi de nome "+heroName+" está no nivel de " +"Bronze")
+}
+else if (xp >= 2000 && xp <= 5000){
+    console.log("O Heroi de nome "+heroName+" está no nivel de " +"Prata")
+}
+else if (xp >= 6000 && xp <= 7000){
+    console.log("O Heroi de nome "+heroName+" está no nivel de " +"ouro")
+}
+else if (xp >= 7000 && xp <= 8000){
+    console.log("O Heroi de nome "+heroName+" está no nivel de " +"Platina Diamante")
+}
+else if (xp >= 8000 && xp <= 9000){
+    console.log("O Heroi de nome "+heroName+" está no nivel de " +"Ascendente")
+}
+else if (xp >= 9000 && xp <= 10000){
+    console.log("Imortal")
+}
+else if (xp >= 10001){
+    console.log("O Heroi de nome "+heroName+" está no nivel de " +"Radiante")
+}
